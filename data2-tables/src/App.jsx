19f379004a757a628427a2320a33f27bb63fccaa
Chgaurav36 <gauravchaurasia619@gmail.com>
2024-09-04 10:58:26 +0530

    data-table

--- data2-tables/src/App.jsx
+++ data2-tables/src/App.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import {
   Table,
   TableBody,
@@ -7,183 +7,172 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Checkbox,
+  Button,
+  Link,
+  IconButton,
 } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
+import { Upload, Delete } from "@mui/icons-material";
+import { styled } from "@mui/system";
 
-function PreExecutionTable() {
-  const rows = [
-    {
-      srNo: 1,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 2,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: false,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 3,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: false,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 4,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 5,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 6,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 7,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 8,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 9,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 10,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 11,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 12,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 13,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 14,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 15,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-    {
-      srNo: 16,
-      stage: "Fencing",
-      activity: "Confirmation from BD",
-      status: true,
-      uploadDocument: "VIEW",
-      action: "Save",
-    },
-  ];
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  border: "2px solid black", // adds border to cells
+  padding: theme.spacing(1),
+  textAlign: "center", // Center-align all text by default
+}));
 
+const ActivityTableCell = styled(StyledTableCell)(({ theme }) => ({
+  textAlign: "left", // Left-align text in the Activity column
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:last-child td, &:last-child th": {
+    borderBottom: "1px solid black",
+  },
+}));
+
+const BlackCheckbox = styled(Checkbox)({
+  color: "#000", // Set color to black
+  "&.Mui-checked": {
+    color: "#000", // Set checked color to black
+  },
+});
+
+const BlackButton = styled(Button)({
+  backgroundColor: "#000", // Set background color to black
+  color: "#fff", // Set text color to white
+  "&:hover": {
+    backgroundColor: "#333", // Set hover color to a darker black
+  },
+});
+
+function createData(
+  srNo,
+  stages,
+  activity,
+  status,
+  upload,
+  action,
+  rowSpan = 1
+) {
+  return { srNo, stages, activity, status, upload, action, rowSpan };
+}
+
+const rows = [
+  createData(1, "Fencing", "Confirmation From BD", true, "Upload", "Save", 3),
+  createData(1, "", "Flat Vacant", true, "VIEW", "Save"),
+  createData(1, "", "Gate Location As Per Vastu", false, "Upload", "Save"),
+  createData(
+    2,
+    "Electric Disconnection",
+    "All Members Vacant",
+    false,
+    "Upload",
+    "Save",
+    4
+  ),
+  createData(2, "", "All Members Light Bill", false, "Upload", "Save"),
+  createData(2, "", "Society Light Bill", false, "Upload", "Save"),
+  createData(2, "", "Application", false, "Upload", "Save"),
+  createData(
+    3,
+    "Water Disconnection",
+    "All Members Vacant",
+    false,
+    "Upload",
+    "Save",
+    3
+  ),
+  createData(3, "", "Water Bill", false, "Upload", "Save"),
+  createData(3, "", "Application", false, "Upload", "Save"),
+  createData(
+    4,
+    "MGL Disconnection",
+    "All Members Vacant",
+    false,
+    "Upload",
+    "Save",
+    3
+  ),
+  createData(4, "", "All Members MGL Bill", false, "Upload", "Save"),
+  createData(4, "", "Application", false, "Upload", "Save"),
+  createData(
+    5,
+    "Meter Cabin",
+    "Temporary Meter Location",
+    false,
+    "Upload",
+    "Save",3
+  ),
+  createData(5, "", "All Members MGL Bill", false, "Upload", "Save"),
+  createData(5, "", "Application", false, "Upload", "Save"),
+ 
+];
+
+export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 1300 }} aria-label="pre-execution table">
-        <TableHead>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{backgroundColor:"#d1d1d1"}}>
           <TableRow>
-            <TableCell>Sr. No.</TableCell>
-            <TableCell>Stages</TableCell>
-            <TableCell>Activity</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Upload Document</TableCell>
-            <TableCell>Action</TableCell>
+            <StyledTableCell sx={{ width: "5%" }}>Sr. No.</StyledTableCell>
+            <StyledTableCell sx={{ width: "19%" }}>Stages</StyledTableCell>
+            <ActivityTableCell sx={{ width: "19%" }}>
+              Activity
+            </ActivityTableCell>
+            <StyledTableCell sx={{ width: "19%" }}>Status</StyledTableCell>
+            <StyledTableCell sx={{ width: "19%" }}>
+              Upload Document
+            </StyledTableCell>
+            <StyledTableCell sx={{ width: "19%" }}>Action</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.srNo}>
-              <TableCell>{row.srNo}</TableCell>
-              <TableCell>{row.stage}</TableCell>
-              <TableCell>
-                <TableRow>{row.activity}</TableRow>
-                <TableRow>{row.activity}</TableRow>
-                <TableRow>{row.activity}</TableRow>
-              </TableCell>
-              <TableCell>
-                <TableRow>
-                  {row.status ? <Checkbox defaultChecked /> : <Checkbox />}
-                </TableRow>
-                <TableRow>
-                  {row.status ? <Checkbox defaultChecked /> : <Checkbox />}
-                </TableRow>
-                <TableRow>
-                  {row.status ? <Checkbox defaultChecked /> : <Checkbox />}
-                </TableRow>
-              </TableCell>
-              <TableCell>{row.uploadDocument}</TableCell>
-              <TableCell>{row.action}</TableCell>
-            </TableRow>
+          {rows.map((row, index) => (
+            <StyledTableRow key={index}>
+              {index === 0 || rows[index - 1].srNo !== row.srNo ? (
+                <>
+                  <StyledTableCell rowSpan={row.rowSpan}>
+                    {row.srNo}
+                  </StyledTableCell>
+                  <StyledTableCell rowSpan={row.rowSpan}>
+                    {row.stages}
+                  </StyledTableCell>
+                </>
+              ) : null}
+              <ActivityTableCell>{row.activity}</ActivityTableCell>
+              <StyledTableCell>
+                <BlackCheckbox checked={row.status} />
+              </StyledTableCell>
+              <StyledTableCell>
+                
+                {row.upload === "VIEW" ? (
+                  <>
+                    <Link href="#" underline="hover" style={{color:"black" }}>
+                      VIEW
+                    </Link>
+                    <IconButton aria-label="delete" size="small" color="error">
+                      <Delete fontSize="inherit" />
+                    </IconButton>
+                  </>
+                ) : (
+                  <IconButton aria-label="upload" size="small">
+                  <Link href="#" underline="hover" sx={{ marginRight: "8px", fontSize:"15px", color:"black" }}>
+                  Upload
+                </Link>
+                    <Upload fontSize="inherit" />
+                  </IconButton>
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                <BlackButton variant="contained" size="small">
+                  Save
+                </BlackButton>
+              </StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
-
-export default PreExecutionTable;
